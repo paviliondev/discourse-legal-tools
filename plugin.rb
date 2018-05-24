96c5cecce5cc0cba270df6563c4f4e88dbd6013c
Angus McLeod <angus@mcleod.org.au>
2018-05-24 16:11:59 +1000

    Add privacy policy wizard template

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,17 @@
 # version: 0.1
 # author: Angus McLeod
 
+DiscourseEvent.on(:custom_wizard_ready) do
+  if defined?(CustomWizard) == 'constant' && CustomWizard.class == Module
+    unless PluginStoreRow.exists?(plugin_name: 'custom_wizard', key: 'privacy_update')
+      CustomWizard::Wizard.add_wizard(File.read(File.join(
+        Rails.root, 'plugins', 'discourse-legal-tools', 'config', 'wizards', 'privacy_update.json'
+      )))
+      SiteSetting.wizard_redirect_exclude_paths += '|privacy'
+    end
+  end
+end
+
 after_initialize do
   load File.expand_path('../lib/export_csv_file_extension.rb', __FILE__)
 
