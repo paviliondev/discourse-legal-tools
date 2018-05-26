2331cba02fcf7a177e61717b5d3cd1f869bd1b13
Angus McLeod <angus@mcleod.org.au>
2018-05-26 17:55:08 +1000

    Remove example wizard

--- plugin.rb
+++ plugin.rb
@@ -3,17 +3,6 @@
 # version: 0.1
 # author: Angus McLeod
 
-DiscourseEvent.on(:custom_wizard_ready) do
-  if defined?(CustomWizard) == 'constant' && CustomWizard.class == Module
-    unless PluginStoreRow.exists?(plugin_name: 'custom_wizard', key: 'privacy_update')
-      CustomWizard::Wizard.add_wizard(File.read(File.join(
-        Rails.root, 'plugins', 'discourse-legal-tools', 'config', 'wizards', 'privacy_update.json'
-      )))
-      SiteSetting.wizard_redirect_exclude_paths += '|privacy'
-    end
-  end
-end
-
 load File.expand_path('../models/legal_extended_user_download_admin_site_setting.rb', __FILE__)
 
 after_initialize do
